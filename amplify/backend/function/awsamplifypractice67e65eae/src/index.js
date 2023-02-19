b10767ff71464d18b0e2b0a888f91eb7b2e19144
Simon Markus <Simon.Markus@verint.com>
2023-02-18 23:16:49 -0600

    Added hosting, added client side react to call backend api endpoint

--- amplify/backend/function/awsamplifypractice67e65eae/src/index.js
+++ amplify/backend/function/awsamplifypractice67e65eae/src/index.js
@@ -1,17 +1,18 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    console.log(event);
+    const customerId = event.pathParameters.customerId;
+    const customer = { customerId: customerId, customerName: "Customer " + customerId}; //Replace with database call
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*"
+      }, 
+        body: JSON.stringify(customer),
     };
 };
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api7cffa6a0": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "awsamplifypractice67e65eae": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
